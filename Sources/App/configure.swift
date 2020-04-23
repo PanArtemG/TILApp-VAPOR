3bb7e59ae06d65f85421c8806e3d597002c2a27a
Artem Panasenko <52529151+PanArtemG@users.noreply.github.com>
2020-04-23 17:23:54 +0300

    add Migration

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,26 +6,26 @@ import SendGrid
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-     
+    
     // Register providers first
     try services.register(FluentPostgreSQLProvider())
     try services.register(LeafProvider())
     try services.register(AuthenticationProvider())
     try services.register(SendGridProvider())
-
-
+    
+    
     // Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
-
+    
     // Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     middlewares.use(SessionsMiddleware.self)
     services.register(middlewares)
-
+    
     // Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
     
@@ -45,7 +45,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     
     databases.add(database: database, as: .psql)
     services.register(databases)
-
+    
     // Configure migrations
     var migrations = MigrationConfig()
     migrations.add(model: User.self, database: DatabaseIdentifier<User.Database>.psql)
@@ -53,9 +53,16 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     migrations.add(model: Category.self, database: DatabaseIdentifier<Category.Database>.psql)
     migrations.add(model: AcronymCategoryPivot.self, database: DatabaseIdentifier<AcronymCategoryPivot.Database>.psql)
     migrations.add(model: Token.self, database: DatabaseIdentifier<Token.Database>.psql)
-    migrations.add(migration: AdminUser.self, database: DatabaseIdentifier<AdminUser.Database>.psql)
+    switch env {
+    case .development, .testing:
+        migrations.add(migration: AdminUser.self, database: DatabaseIdentifier<AdminUser.Database>.psql)
+    default:
+        break
+    }
+    
     migrations.add(migration: ResetPasswordToken.self, database: DatabaseIdentifier<ResetPasswordToken.Database>.psql)
-
+    migrations.add(migration: MakeCategoriesUnique.self, database: DatabaseIdentifier<MakeCategoriesUnique.Database>.psql)
+    
     services.register(migrations)
     
     var commandConfig = CommandConfig.default()
@@ -66,7 +73,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     config.prefer(MemoryKeyedCache.self, for: KeyedCache.self)
     
     guard let sendGridAPIKey = Environment.get("SENDGRID_API_KEY") else {
-      fatalError("No Send Grid API Key specified")
+        fatalError("No Send Grid API Key specified")
     }
     let sendGridConfig = SendGridConfig(apiKey: sendGridAPIKey)
     services.register(sendGridConfig)
